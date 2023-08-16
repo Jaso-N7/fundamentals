04e8944ff6e27f1bc60d5f86d108b6b468dc248a
Jason Robinson <jasonsrobinson@protonmail.com>
2023-08-16 07:58:38 -0500

    Practical Exercise 2: Create a method in the EmployeeUtilities class called validateEmployeeId.

--- EmployeeExpensesManagement/src/com/mindfulengineering/expenses/utilities/EmployeeUtilities.java
+++ EmployeeExpensesManagement/src/com/mindfulengineering/expenses/utilities/EmployeeUtilities.java
@@ -6,6 +6,7 @@ package com.mindfulengineering.expenses.utilities;
 
 import com.mindfulengineering.expenses.domain.Employee;
 import com.mindfulengineering.expenses.domain.Employees;
+import com.mindfulengineering.expenses.exceptions.InvalidEmployeeIdException;
 import java.util.Objects;
 
 /**
@@ -20,5 +21,31 @@ public class EmployeeUtilities {
         Objects.requireNonNull(emp, "A valid Employee must be provided");
         
         return (emps.findBySurname(emp.getSurname()) != null);
+    }
+    
+    /**
+     * Returns an Integer object holding the value of the Employee ID.
+     * 
+     * @param empId - The Employee ID to be parsed
+     * @return An Integer object holding the Employee ID represented by the string
+     * argument.
+     * @throws InvalidEmployeeIdException if the string cannot be recognized as a
+     * valid Employee ID.
+     */
+    public static Integer validateEmployeeId (String empId)
+            throws InvalidEmployeeIdException {
+        
+        Objects.requireNonNull(empId, "An employee id is required");
+        
+        if (empId.isBlank() || empId.isEmpty()) {
+            throw new InvalidEmployeeIdException();
+        }
+        
+        try {
+            Integer eId = Integer.valueOf(empId);
+            return eId;
+        } catch (NumberFormatException nfe) {
+            throw new InvalidEmployeeIdException();
+        }
     }
 }

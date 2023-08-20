3811742cec763f9efd4d96e74b762032fc59e2e1
Jason Robinson <jasonsrobinson@protonmail.com>
2023-08-20 12:36:32 -0500

    Printing to console instead of throwing exception
    Does not break the flow of the application, especially when testing

--- EmployeeExpensesManagement/src/com/mindfulengineering/expenses/utilities/ExpenseAnalysisTempImpl.java
+++ EmployeeExpensesManagement/src/com/mindfulengineering/expenses/utilities/ExpenseAnalysisTempImpl.java
@@ -10,17 +10,17 @@ public class ExpenseAnalysisTempImpl implements ExpenseAnalysis {
 
     @Override
     public void printOutstandingExpenseClaims() {
-        throw new UnsupportedOperationException("this feature is not currently available");
+        System.out.println("this feature is not currently available");
     }
 
     @Override
     public void printPaidExpenseClaims(LocalDate start, LocalDate end) {
-        throw new UnsupportedOperationException("this feature is not currently available");
+        System.out.println("this feature is not currently available");
     }
 
     @Override
     public void printExpenseClaims(double aboveAmount) {
-        throw new UnsupportedOperationException("this feature is not currently available");
+        System.out.println("this feature is not currently available");
     }
     
 }
